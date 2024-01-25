cf02633e1025887dc5ffe74d046e9ea1b52ef323
Olga Stavrova <ol.stavrova@yandex.by>
2024-01-25 12:53:11 +0300

    Файл index.js (работающий код) до разделения на модули card.js и modal.js.

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -8,65 +8,105 @@ const cardTemplate = document.querySelector("#card-template").content;
 
 const placesList = document.querySelector(".places__list");
 
-// @todo: Функция создания карточки
 
-function createCard(cardData, deleteCard) {
+// @todo: Функция создания карточки
+//card.js
+function createCard(cardData, deleteCardFunction, likeCardFunction) {
   const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
 
   const cardImage = cardElement.querySelector(".card__image");
   cardImage.src = cardData.link;
   cardImage.alt = cardData.name;
   cardElement.querySelector(".card__title").textContent = cardData.name;
+  cardImage.addEventListener("click", openImageModal);
 
   const deleteButton = cardElement.querySelector(".card__delete-button");
-  deleteButton.addEventListener("click", deleteCard);
+  deleteButton.addEventListener("click", deleteCardFunction);
 
+  const likeButton = cardElement.querySelector('.card__like-button');
+  likeButton.addEventListener('click', likeCardFunction);
   return cardElement;
 }
 
 // @todo: Функция удаления карточки
-
-const onDeleteClick = (clickEvent) => {
+//card.js
+function onDeleteClick (clickEvent) {
   const listItem = clickEvent.target.closest(".card");
   listItem.remove();
 };
 
-// @todo: Вывести карточки на страницу
+//card.js
+//функция лайка карточки
+function onLikeClick (clickEvent) {
+  clickEvent.target.classList.toggle('card__like-button_is-active');
+};
 
+// @todo: Вывести карточки на страницу
+//index.js
 function renderCards(cardsList) {
   cardsList.forEach(function (cardData) {
-    const cardElement = createCard(cardData, onDeleteClick);
+    const cardElement = createCard(cardData, onDeleteClick, onLikeClick);
     placesList.appendChild(cardElement);
   });
 }
 
 renderCards(initialCards);
 
-
+//modal.js
 //открытие модального окна формы
 const editProfileForm = document.querySelector('.popup_type_edit');
-const closeModal = editProfileForm.querySelector('.popup__close');
+const closeProfileForm = editProfileForm.querySelector('.popup__close');
 const addCardForm = document.querySelector('.popup_type_new-card');
-const clickClosedButton = addCardForm.querySelector('.popup__close');
+const closeCardForm = addCardForm.querySelector('.popup__close');
 const overlays = document.querySelectorAll('.popup');
+const imagePopup = document.querySelector('.popup_type_image');
+const closeImagePopup = imagePopup.querySelector('.popup__close');
+const imagePopupContent = imagePopup.querySelector('.popup__image');
+const imagePopupCaption = imagePopup.querySelector('.popup__caption');
 
-/*
-const onClickCloseEditForm = (clickEvent) => {
-  //const modalForm = clickEvent.target.closest(".popup_type_edit");
-  editProfileForm.classList.remove('popup_is-opened');
+
+function openProfileModal() {
+  editProfileForm.classList.add('popup_is-opened');
+  editProfileForm.classList.toggle('popup_is-animated');
+  closeProfileForm.addEventListener('click', onClickCloseProfileForm);
+  document.addEventListener('keydown', handleEscClose);
 };
-*/
-function onClickCloseEditForm(clickEvent) {
-  //const modalForm = clickEvent.target.closest(".popup_type_edit");
+
+function onClickCloseProfileForm() {
   editProfileForm.classList.remove('popup_is-opened');
-  closeModal.removeEventListener('click', onClickCloseEditForm);
+  closeProfileForm.removeEventListener('click', onClickCloseProfileForm);
   document.removeEventListener('keydown', handleEscClose);
 };
 
-function onClickCloseCardForm(clickEvent) {
- // const modalForm = clickEvent.target.closest(".popup_type_new-card");
+function openCardModal() {
+  addCardForm.classList.add('popup_is-opened');
+  addCardForm.classList.toggle('popup_is-animated');
+  closeCardForm.addEventListener('click', onClickCloseCardForm);
+  document.addEventListener('keydown', handleEscClose);
+};
+
+function onClickCloseCardForm() {
   addCardForm.classList.remove('popup_is-opened');
-  clickClosedButton.removeEventListener('click', onClickCloseCardForm);
+  closeCardForm.removeEventListener('click', onClickCloseCardForm);
+  document.removeEventListener('keydown', handleEscClose);
+};
+
+
+function openImageModal(clickEvent) {
+  imagePopup.classList.add('popup_is-opened');
+  imagePopup.classList.toggle('popup_is-animated');
+  const currentCard = clickEvent.target.closest('.card');
+  const cardTitle = currentCard.querySelector('.card__title');
+  const cardImage = currentCard.querySelector('.card__image');
+  imagePopupCaption.textContent = cardTitle.textContent;
+  imagePopupContent.src = cardImage.src;
+  closeImagePopup.addEventListener('click', onClickCloseImage);
+  document.addEventListener('keydown', handleEscClose);
+};
+
+function onClickCloseImage() {
+  imagePopup.classList.remove('popup_is-opened');
+  closeImagePopup.removeEventListener('click', onClickCloseImage);
   document.removeEventListener('keydown', handleEscClose);
 };
 
@@ -75,41 +115,28 @@ function handleEscClose(event) {
     const openedPopup = document.querySelector('.popup_is-opened');
     closePopup(openedPopup);
   }
-}
-
-function openProfileModal() {
-editProfileForm.classList.add('popup_is-opened');
-closeModal.addEventListener('click', onClickCloseEditForm);
-document.addEventListener('keydown', handleEscClose);
-}
-
-function openCardModal() {
-addCardForm.classList.add('popup_is-opened');
-clickClosedButton.addEventListener('click', onClickCloseCardForm);
-document.addEventListener('keydown', handleEscClose);
-}
-
-function closePopup(popup) {
-  popup.classList.remove('popup_is-opened');
-}
-
+};
 
 function handleOverlayClose(event) {
   if (event.target === event.currentTarget) {
     const popup = event.target.closest('.popup');
     closePopup(popup);
   }
-}
+};
 
 overlays.forEach((popup) => {
   popup.addEventListener('click', handleOverlayClose);
 });
 
+function closePopup(popup) {
+  popup.classList.remove('popup_is-opened');
+};
 
-let clickProfileButton = document.querySelector('.profile__edit-button');
+
+const clickProfileButton = document.querySelector('.profile__edit-button');
 clickProfileButton.addEventListener('click', openProfileModal);
 
-let clickCardButton = document.querySelector('.profile__add-button');
+const clickCardButton = document.querySelector('.profile__add-button');
 clickCardButton.addEventListener('click', openCardModal);
 
 // Находим форму в DOM
@@ -123,8 +150,8 @@ jobInput.value = document.querySelector('.profile__description').textContent;
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function handleFormSubmit(evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+function handleFormSubmit(event) {
+  event.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
                                                 // Так мы можем определить свою логику отправки.
                                                 // О том, как это делать, расскажем позже.
 
@@ -134,7 +161,7 @@ function handleFormSubmit(evt) {
     document.querySelector('.profile__title').textContent = nameInput.value;
     document.querySelector('.profile__description').textContent = jobInput.value;
 
-    onClickCloseEditForm(evt);
+    onClickCloseProfileForm(event);
   
 }
 
@@ -148,8 +175,8 @@ const cardUrlInput = addCardForm.querySelector('.popup__input_type_url');
 
 // Обработчик «отправки» формы, хотя пока
 // она никуда отправляться не будет
-function handleCardFormSubmit(evt) {
-    evt.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
+function handleCardFormSubmit(event) {
+  event.preventDefault(); // Эта строчка отменяет стандартную отправку формы.
     let newCard = {name:cardNameInput.value, link:cardUrlInput.value};
 
     initialCards.unshift(newCard);
@@ -157,10 +184,12 @@ function handleCardFormSubmit(evt) {
     //placesList.appendChild(cardElement);
     placesList.insertBefore(cardElement, placesList.firstChild);
 
-    onClickCloseCardForm(evt);
+    onClickCloseCardForm(event);
   
 }
 
 // Прикрепляем обработчик к форме:
 // он будет следить за событием “submit” - «отправка»
 addCardForm.addEventListener('submit', handleCardFormSubmit); 
+
+
